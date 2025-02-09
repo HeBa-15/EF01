d52509e009e0a46ea2f5fdd5cb4954af1609ce03
HeBa-15 <hebamahmoudali1510@gmail.com>
2025-02-10 01:00:18 +0200

    Ways Of Mapping (By Convention)

---
+++ EF01/Entities/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF01.Entities
+{
+
+    // EF Core 4 Ways for Mapping (Table , Function ,View)
+    // 1. By Convention (Default)
+    // 2. By Data Annotation
+
+
+
+
+    // Poco Class => Plained Old CLR Object Class
+
+    #region By Convention (Default)
+    internal class Employee
+    {
+        public int Id { get; set; }// public Numeric Property 'Id' || 'EmployeeId' => // PK Identity (1,1)
+        public string? Name { get; set; } // Reference Type : string => nvarchar(Max) Not Req
+        public int? Age { get; set; } //Value Type : int => Nullable<int>
+        
+        public decimal Salary { get; set; } // Float
+
+    
+    } 
+
+    #endregion
+}
