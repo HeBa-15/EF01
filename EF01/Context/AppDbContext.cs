6aaa0b979147bc4c8cff9374b6debb77b37aad06
HeBa-15 <hebamahmoudali1510@gmail.com>
2025-02-10 13:37:41 +0200

    DbContext

---
+++ EF01/Context/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF01.Context
+{
+    internal class AppDbContext: Microsoft.EntityFrameworkCore.DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "server=.;database =EFCoreDbGroup01;Trusted_Connection =true;";
+            
+            optionsBuilder.UseSqlServer(connectionString);
+        
+        }
+    }
+}
--- EF01/Program.cs
+++ EF01/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace EF01
+﻿using EF01.Context;
+
+namespace EF01
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            using (var context =new AppDbContext())
+            {
+
+            }
         }
     }
 }
